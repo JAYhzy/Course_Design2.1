30849e2351a1d5db6a8b89017ed900b0533fcea9
Jay_hzy <1820646630@qq.com>
2023-01-06 13:31:06 +0800

    2.0

--- src/users/Admin.java
+++ src/users/Admin.java
@@ -15,23 +15,13 @@ public class Admin extends User{
         System.out.println("0.quit");
     }
     public  void Operation(int n,BookList bookList)throws OperationException{
-        IOperation iOperation=null;
-        switch(n){
-            case 0:
-                iOperation= new ExitOperation();
-                break;
-            case 1:
-                iOperation=new FindOperation();
-                break;
-            case 2:
-                iOperation=new AddOperation();
-                break;
-            case 3:
-                iOperation= new DisplayOperation();
-                break;
-            default:
-                throw new OperationException("Input errors, please re-enter");
-        }
+        IOperation iOperation = switch (n) {
+            case 0 -> new ExitOperation();
+            case 1 -> new FindOperation();
+            case 2 -> new AddOperation();
+            case 3 -> new DisplayOperation();
+            default -> throw new OperationException("Input errors, please re-enter");
+        };
         try {
             iOperation.work(bookList);
         }catch(OperationException e){
--- src/users/Reader.java
+++ src/users/Reader.java
@@ -12,28 +12,14 @@ public class Reader extends User{
     public void menu() {
         System.out.println("dear:"+this.getName()+ "welcome to library");
         System.out.println("1.search");
-        System.out.println("2.borrow");
-        System.out.println("3.return");
         System.out.println("0.quit");
     }
     public void Operation(int n, BookList bookList)throws OperationException {
-        IOperation iOperation=null;
-        switch(n){
-            case 0:
-                iOperation= new ExitOperation();
-                break;
-            case 1:
-                iOperation=new FindOperation();
-                break;
-            case 2:
-                //iOperation=new BorrowOperation();
-                break;
-            case 3:
-                //iOperation=new ReturnOperation();
-                break;
-            default:
-                throw new OperationException("Input errors, please re-enter");
-        }
+        IOperation iOperation = switch (n) {
+            case 0 -> new ExitOperation();
+            case 1 -> new FindOperation();
+            default -> throw new OperationException("Input errors, please re-enter");
+        };
         try {
             iOperation.work(bookList);
         }catch(OperationException e){
